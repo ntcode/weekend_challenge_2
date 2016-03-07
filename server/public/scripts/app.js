3ba5d9c4b9ae956766ffe9a3da12c8518d7341a0
ntcode <ntskupa@gmail.com>
2016-03-07 09:23:56 -0600

    first commit

---
+++ server/public/scripts/app.js
@@ -0,0 +1,68 @@
+$(document).ready(function(){
+    $.ajax({
+      type: "GET",
+      url: "/data",
+      success: function(data){
+        console.log(data);
+
+var kappa = [];
+var index = 0;
+var time = 20000;
+  for(var i = 0; i < data.kappa.length; i++) {
+    kappa.push(data.kappa[i]);
+  }
+//sets the individual that is being viewed
+setInterval(function(){
+  $('.people').children().remove();
+  $('.people').append(
+    '<div>' + "<b>Name: </b>" + kappa[index].name + '</div>' +
+    '<div>' + "<b>From: </b>" + kappa[index].location + '</div>' +
+    '<div>' + "<b>Spirit Animal: </b>" + kappa[index].spirit_animal + '</div>' +
+    '<div>' + "<b>Shoutout: </b>" + kappa[index].shoutout + '</div>');
+
+  index++;
+  if (index > kappa.length -1) {
+    index = 0;
+  }
+}, time);
+//click previous button - previous kappa
+$('.click').on('click', '.prev', function(){
+  index--;
+  time = 20000;
+  if(index == -1) {
+    index = kappa.length -1;
+  }
+  $('.people').children().remove();
+  $('.people').append(
+    '<div>' + "<b>Name: </b>" + kappa[index].name + '</div>' +
+    '<div>' + "<b>From: </b>" + kappa[index].location + '</div>' +
+    '<div>' + "<b>Spirit Animal: </b>" + kappa[index].spirit_animal + '</div>' +
+    '<div>' + "<b>Shoutout: </b>" + kappa[index].shoutout + '</div>');
+});
+//click next button - next kappa
+$('.click').on('click', '.next', function(){
+  index++;
+  time = 20000;
+  if(index > kappa.length -1) {
+    index = 0;
+  }
+  $('.people').children().remove();
+  $('.people').append(
+    '<div>' + "<b>Name: </b>" + kappa[index].name + '</div>' +
+    '<div>' + "<b>From: </b>" + kappa[index].location + '</div>' +
+    '<div>' + "<b>Spirit Animal: </b>" + kappa[index].spirit_animal + '</div>' +
+    '<div>' + "<b>Shoutout: </b>" + kappa[index].shoutout + '</div>');
+});
+
+}
+});
+});
+
+
+
+
+
+
+
+
+//
